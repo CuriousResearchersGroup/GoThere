ad45d6786bc0e65b7f65802945755668ab5ef503
Admin <admin@Mac-Admin.local>
2018-05-05 22:17:09 +0300

    New HW on Swift

---
+++ GoThere_sw/GoThere/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoThere
+//
+//  Created by Admin on 05.05.18.
+//  Copyright © 2018 G4PLAY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
